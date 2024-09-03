c2ebea3e82ddefcd8f84ef032905a3eaf33b29f0
fami0110 <masandofami@gmail.com>
2024-09-03 10:42:05 +0700

    fix esreveR

--- Seleksi 2024/Reverse/egneveR esreveR/esreveR.py
+++ Seleksi 2024/Reverse/egneveR esreveR/esreveR.py
@@ -1,19 +1,18 @@
+text = open('poem.txt', 'r')
 dist = open('dist.txt', 'w')
 flag = open('flag.txt', 'w')
-redacted = open('redacted.txt', 'r')
 
-
-for line in redacted:
+for line in text:
     if line == '\n':
         dist.write('\n')
         continue
     dist.write(" ".join(word[::-1] for word in line.strip().split(' ')) + "\n")
 
 
-FLAG = "ctfgrf24{C4p7ur3_Th3_F149_24}"
+FLAG = "ctfgrf24{REDACTED}"
 i = 0
 
-for j, char in enumerate(redacted.read()):
+for j, char in enumerate(text.read()):
     if i == len(FLAG):
         break
     if char == FLAG[i]:
